1cbc39b9a435dfdd5c9d23dd032cc4c90c4b2a70
Agata Makarewicz <43831153+amakarewicz@users.noreply.github.com>
2023-01-10 00:22:10 +0100

    s1 exec additional files

--- s1_development_environment/exercise_files/final_exercise/model.py
+++ s1_development_environment/exercise_files/final_exercise/model.py
@@ -1,6 +1,25 @@
 from torch import nn
+import torch.functional as F
 
 
 class MyAwesomeModel(nn.Module):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, input_size, output_size, hidden_layers, drop_p=0.5):
+        super().__init__()    
+        
+        self.hidden_layers = nn.ModuleList([nn.Linear(input_size, hidden_layers[0])])
+
+        layer_sizes = zip(hidden_layers[:-1], hidden_layers[1:])
+        self.hidden_layers.extend([nn.Linear(h1, h2) for h1, h2 in layer_sizes])
+
+        self.output = nn.Linear(hidden_layers[-1], output_size)
+
+        self.dropout = nn.Dropout(p=drop_p)
+
+    def forward(self, x):
+
+        for layer in self.hidden_layers:
+            x = F.relu(layer(x))
+            x = self.dropout(x)
+        x = self.output(x)
+
+        return F.log_softmax(x, dim=1)
---
+++ s1_development_environment/test_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# A sample Bash script, by Agata
+for i in {1..5}
+do
+    python3 test_script.py
+done
