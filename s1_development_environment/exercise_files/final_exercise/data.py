7f3c3639ec44127681777488257afd43f6fb75c6
Agata Makarewicz <43831153+amakarewicz@users.noreply.github.com>
2023-01-20 16:33:57 +0100

    update

--- s1_development_environment/exercise_files/final_exercise/data.py
+++ s1_development_environment/exercise_files/final_exercise/data.py
@@ -1,21 +1,37 @@
-import numpy as np
 import torch
+from torch.utils.data import Dataset, ConcatDataset, DataLoader
+import numpy as np
+import os
 
-
-def mnist():
-    folder_path = "./../../../data/corruptmnist"    
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784) 
-    path = '/tmp/data/mnist.npz'
-
-    with np.load(path) as f:
+class MyDataset(Dataset):
+    def __init__(self, images: torch.Tensor, labels: torch.Tensor, allow_pickle: torch.Tensor):
+        self.images = images
+        self.labels = labels
+        self.allow_pickle = allow_pickle
         
-        x_train, y_train = f['x_train'], f['y_train']
-        x_test, y_test = f['x_test'], f['y_test']
-        return train, test
+    def __getitem__(self, index):
+        image = self.images[index]
+        label = self.labels[index]
+        
+        return [image, label]
+    
+    def __len__(self):
+        return len(self.images)
 
-    def load_data(path):
-        with np.load(path) as f:
-            x_train, y_train = f['x_train'], f['y_train']
-            x_test, y_test = f['x_test'], f['y_test']
-            return (x_train, y_train), (x_test, y_test)
+def mnist(directory: str = 'data/corruptmnist'):
+    
+    train_datasets = []
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        data = np.load(f)
+        data = dict(zip(("{}".format(item) for item in data), (torch.from_numpy(data[item]) for item in data)))
+        dataset = MyDataset(data['images'].to(torch.float32), data['labels'], data['allow_pickle'])
+        if 'train' in filename:
+            train_datasets.append(dataset)
+        else: test = dataset
+        
+    train = ConcatDataset(train_datasets)
+    train = DataLoader(train, batch_size=64, shuffle=True)
+    test = DataLoader(test, batch_size=64, shuffle=True)
+    
+    return train, test
--- s2_organisation_and_version_control/exercise_files/typing_exercise.py
+++ s2_organisation_and_version_control/exercise_files/typing_exercise.py
@@ -60,7 +60,7 @@ def validation(model: nn.Module,
         # Class with highest probability is our predicted class, compare with true label
         equality = (labels.data == ps.max(1)[1])
         # Accuracy is number of correct predictions divided by all predictions, just take the mean
-        accuracy += equality.type_as(torch.FloatTensor()).mean()
+        accuracy += equality.type_as(torch.FloatTensor()).mean().item() # item() fixes the errors !!!
 
     return test_loss, accuracy
 
